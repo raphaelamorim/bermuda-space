5e658ba49fd14741f357579c6b60bf2622a5cc5c
chaintan17 <chaintan17@gmail.com>
2016-08-06 12:55:23 +0700

    add react facebook

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,38 @@ import {
   Text,
   View
 } from 'react-native';
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  AccessToken
+} = FBSDK;
+
+var Login = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
+    );
+  }
+});
 
 class BermudaSpace extends Component {
   render() {
@@ -26,6 +58,7 @@ class BermudaSpace extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <Login />
       </View>
     );
   }
